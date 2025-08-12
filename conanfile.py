84b591f3203266281198ebffa13764d2be8ab892
Igors Volohovs <igorsvolohovs@gmail.com>
2025-08-12 08:43:16 -0700

    bounded_queue testing

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,25 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeToolchain
+from conan.tools.build import check_min_cppstd
 
 
 class ExampleRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    # Убираем "CMakeToolchain" из generators.
+    # Conan сам сгенерирует его в методе generate.
+    generators = "CMakeDeps" 
 
     def requirements(self):
         self.requires("gtest/1.16.0")
 
     def layout(self):
         cmake_layout(self)
+        
+    def validate(self):
+        check_min_cppstd(self, "23")
+
+    def generate(self):
+        # Создаем и настраиваем CMakeToolchain только здесь
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = "ON"
+        tc.generate()
--- tests/queue/bounded_queue.cpp
+++ tests/queue/bounded_queue.cpp
@@ -3,3 +3,13 @@
 #include "queue/bounded_queue.hpp"
 
 // здесь ваш код
+
+// test BoundedQueue
+    // test min/max bound
+// test push
+    // test empty
+    // test non-empty
+    // test more then bounded
+// test try_pop
+    // test when exists
+    // test when not exists
